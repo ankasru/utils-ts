4594c10733d6650fac99a00a25a4232dead8c3e7
ryinner <vinograd18062002@gmail.com>
2023-10-09 12:47:13 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Входная точка для всех сервисов
+import * as array from './utils/array.utils';
 import * as async from './utils/async.utils';
 import * as date from './utils/date.utils';
 import * as helpers from './utils/helpers.utils';
@@ -6,4 +7,4 @@ import * as system from './utils/system.utils';
 import * as text from './utils/text.utils';
 import * as url from './utils/url.utils';
 
-export { async, date, helpers, system, text, url };
+export { array, async, date, helpers, system, text, url };
