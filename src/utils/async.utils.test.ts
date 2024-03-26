4aa2bdf956247962bb25b5132ca273493771828d
ryinner <vinograd18062002@gmail.com>
2024-03-26 12:43:03 +0500

    setCustomInterval func

--- src/utils/async.utils.test.ts
+++ src/utils/async.utils.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
-import { debounce, throttle, wait } from './async.utils';
+import { debounce, setCustomInterval, throttle, wait } from './async.utils';
 
 describe('wait', () => {
     beforeEach(() => {
@@ -51,6 +51,46 @@ describe('wait', () => {
         void func(4);
         vi.advanceTimersByTime(300);
         expect(spy).toHaveBeenCalledTimes(2);
+    });
+
+    test('setCustomInterval base', () => {
+        const spy = vi.spyOn(window, 'setInterval');
+
+        setCustomInterval(() => {}, 1000);
+
+        vi.advanceTimersByTime(1000);
+
+        expect(spy).toHaveBeenCalledOnce();
+    });
+
+    test('setCustomInterval times', () => {
+        const spy = vi.fn();
+
+        setCustomInterval(spy, 1000, {
+            times: 4
+        });
+
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+
+        expect(spy).toHaveBeenCalledTimes(4);
+    });
+
+    test('setCustomInterval initial invoke', () => {
+        const spy = vi.fn();
+
+        setCustomInterval(spy, 1000, {
+            initialInvoke: true
+        });
+
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+        vi.advanceTimersByTime(1000);
+
+        expect(spy).toHaveBeenCalledTimes(4);
     });
 
     afterEach(() => {
--- src/utils/async.utils.ts
+++ src/utils/async.utils.ts
@@ -1,13 +1,23 @@
-import type { Callback, CallbackGeneric } from './helpers.utils';
+import { toBoolean, type Callback, type CallbackGeneric } from './helpers.utils';
 
 export async function wait (ms: number): Promise<void> {
-    await new Promise(resolve => { setTimeout(resolve, ms); });
+    await new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
-export function debounce <T extends CallbackGeneric> ({ callback, timeout = 300 }: { callback: T; timeout?: number }): Callback<T> {
+export function debounce<T extends CallbackGeneric> ({
+    callback,
+    timeout = 300
+}: {
+    callback: T;
+    timeout?: number;
+}): Callback<T> {
     let timer: ReturnType<typeof setTimeout> | undefined;
 
-    const debounced = async (...args: Parameters<T>): Promise<ReturnType<T>> => {
+    const debounced = async (
+        ...args: Parameters<T>
+    ): Promise<ReturnType<T>> => {
         return await new Promise<ReturnType<T>>((resolve) => {
             clearTimeout(timer);
             timer = setTimeout(() => {
@@ -20,18 +30,63 @@ export function debounce <T extends CallbackGeneric> ({ callback, timeout = 300 
     return debounced;
 }
 
-export function throttle <T extends CallbackGeneric> ({ callback, timeout = 300 }: { callback: T; timeout?: number }): Callback<T> {
+export function throttle<T extends CallbackGeneric> ({
+    callback,
+    timeout = 300
+}: {
+    callback: T;
+    timeout?: number;
+}): Callback<T> {
     let timer: ReturnType<typeof setTimeout> | undefined;
 
     const throttled: Callback<T> = async (...args) => {
         return await new Promise<ReturnType<T>>((resolve) => {
             if (timer === undefined || timer === null) {
                 // eslint-disable-next-line n/no-callback-literal
-                resolve(<ReturnType<T>> callback(...args));
-                timer = setTimeout(() => { clearTimeout(timer); timer = undefined; }, timeout);
+                resolve(<ReturnType<T>>callback(...args));
+                timer = setTimeout(() => {
+                    clearTimeout(timer);
+                    timer = undefined;
+                }, timeout);
             }
         });
     };
 
     return throttled;
+}
+
+export function setCustomInterval<T extends CallbackGeneric> (
+    callback: T,
+    timeout: number,
+    {
+        times,
+        initialInvoke
+    }: {
+        times?: number | false;
+        initialInvoke?: boolean;
+    } = {}
+): ReturnType<typeof setInterval> {
+    const isCounter = toBoolean(times);
+    let counter = 0;
+
+    if (initialInvoke ?? false) {
+        invokeCallback();
+    }
+
+    const intervalId = setInterval(invokeCallback, timeout);
+
+    return intervalId;
+
+    function invokeCallback (): void {
+        if (isCounter) {
+            if (counter < <number>times) {
+                callback();
+            } else {
+                clearInterval(intervalId);
+            }
+            counter++;
+        } else {
+            callback();
+        }
+    };
 }
